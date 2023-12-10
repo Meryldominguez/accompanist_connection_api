14aaa2665bf832dfb7ba299e2d7ce39393052e49
meryldominguez <meryldominguez@gmail.com>
2023-12-10 12:58:08 -0600

    testing for user controller and model

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -1,38 +1,50 @@
 require "test_helper"
 
 class UsersControllerTest < ActionDispatch::IntegrationTest
-  # setup do
-  #   @user = users(:one)
-  # end
+  setup do
+    @user = users(:one)
+    @user_count = users.count
+  end
 
-  # test "should get index" do
-  #   get users_url, as: :json
-  #   assert_response :success
-  # end
+  test "should get index" do
+    get users_url, as: :json
+    assert_response :success
+  end
 
-  # test "should create user" do
-  #   assert_difference("User.count") do
-  #     post users_url, params: { user: { email: @user.email, first_name: @user.first_name, last_name: @user.last_name } }, as: :json
-  #   end
+  test "should create user" do
+    @new_user = { email: 'alex@gmail.com', first_name: 'Alex', last_name: 'Nowakowski' } 
 
-  #   assert_response :created
-  # end
+    assert_difference("User.count", 1) do
+      post users_url, params: { user: @new_user }, as: :json
+    end
+    assert_response :created
+  end
 
-  # test "should show user" do
-  #   get user_url(@user), as: :json
-  #   assert_response :success
-  # end
+  test "should show user" do
+    get user_url(@user), as: :json
+    assert_response :success
 
-  # test "should update user" do
-  #   patch user_url(@user), params: { user: { email: @user.email, first_name: @user.first_name, last_name: @user.last_name } }, as: :json
-  #   assert_response :success
-  # end
+    response_user = JSON.parse(response.body).with_indifferent_access
+    assert_equal response_user[:id], @user.id
+    assert_equal response_user[:first_name], @user.first_name
+  end
 
-  # test "should destroy user" do
-  #   assert_difference("User.count", -1) do
-  #     delete user_url(@user), as: :json
-  #   end
+  test "should update user" do
+    new_email = "new@email.com"
+    
+    patch user_url(@user), params: { user: { email: new_email } }, as: :json
+    assert_response :success
 
-  #   assert_response :no_content
-  # end
+    response_user = JSON.parse(response.body).with_indifferent_access
+    assert_equal response_user[:email], new_email
+    assert_equal User.find(@user.id).email, new_email
+  end
+
+  test "should destroy user" do
+    assert_difference("User.count", -1) do
+      delete user_url(@user), as: :json
+    end
+
+    assert_response :no_content
+  end
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,34 @@
 require "test_helper"
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @user = users(:one)
+  end
+
+  test "user should be valid" do
+    assert @user.valid?
+  end
+
+  test "first_name should not be blank" do
+    @user.first_name = nil
+    assert_not @user.valid?
+  end
+  test "last_name should not be blank" do
+    @user.last_name = nil
+    assert_not @user.valid?
+  end
+  test "email should not be blank" do
+    @user.email = nil
+    assert_not @user.valid?
+  end
+  test "email should be formatted correctly" do
+    @user.email = "notanemail"
+    assert_not @user.valid?
+  end
+
+  test "email should not be duplicate" do
+    @new_user = users(:two)
+    @user.email = @new_user.email
+    assert_not @user.valid?
+  end
 end
