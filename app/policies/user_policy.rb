20d056e0ad6c3bdaa669ff7f13ba04fd8349507d
meryldominguez <meryldominguez@gmail.com>
2023-12-14 17:55:40 -0600

    rubocop

--- app/policies/user_policy.rb
+++ app/policies/user_policy.rb
@@ -2,20 +2,24 @@
 
 class UserPolicy < ApplicationPolicy
   def initialize(current_user, user)
-    raise Pundit::NotAuthorizedError, "must be logged in" unless current_user
+    super
+    raise Pundit::NotAuthorizedError, 'must be logged in' unless current_user
+
     @current_user = current_user
     @user = user
   end
 
   def update?
-    @current_user.admin? || is_same_user?
+    @current_user.admin? || same_user?
   end
 
   def destroy?
-    @current_user.admin? || is_same_user?
+    @current_user.admin? || same_user?
   end
+
   private
-    def is_same_user?
+
+  def same_user?
     @current_user.id == @user.id
-    end
+  end
 end
--- spec/requests/user_spec.rb
+++ spec/requests/user_spec.rb
@@ -74,77 +74,76 @@ RSpec.describe 'Users', type: :request do
     end
   end
   describe 'PATCH/PUT #update' do
-  context 'with an authenticated user' do
-    context 'who is an admin' do
-      let!(:user) { create(:user) }
-      let!(:admin_user) { create(:user, :admin_user) }
-      context 'updating an account' do
-        before do
-          @new_email = "new@email.com"
-          patch user_path(user.id),
-            params: { email: @new_email },
-            headers: create_auth_header(admin_user)
-
+    context 'with an authenticated user' do
+      context 'who is an admin' do
+        let!(:user) { create(:user) }
+        let!(:admin_user) { create(:user, :admin_user) }
+        context 'updating an account' do
+          before do
+            @new_email = 'new@email.com'
+            patch user_path(user.id),
+                  params: { email: @new_email },
+                  headers: create_auth_header(admin_user)
+          end
+          it 'should return a 200 status' do
+            expect(response).to have_http_status(:ok)
+          end
+          it 'should have the updated user in the body' do
+            expect(JSON(response.body)['data']['attributes']['email']).to eq @new_email
+          end
+          it 'should update a user' do
+            expect(User.find(user.id).email).to eq @new_email
+          end
         end
-        it 'should return a 200 status' do
-          expect(response).to have_http_status(:ok)
+      end
+      context 'who is not an admin' do
+        let!(:user) { create(:user) }
+        context 'editing their own account' do
+          before do
+            @new_email = 'new@email.com'
+            patch user_path(user.id),
+                  params: { email: 'new@email.com' },
+                  headers: create_auth_header(user)
+          end
+          it 'should return a 200 status' do
+            expect(response).to have_http_status(:ok)
+          end
+          it 'should have the updated user in the body' do
+            expect(JSON(response.body)['data']['attributes']['email']).to eq @new_email
+          end
+          it 'should update a user' do
+            expect(User.find(user.id).email).to eq @new_email
+          end
         end
-        it 'should have the updated user in the body' do
-          expect(JSON(response.body)['data']['attributes']['email']).to eq @new_email
-        end
-        it 'should update a user' do
-          expect(User.find(user.id).email).to eq @new_email
+        context "updating someone else's account" do
+          let!(:user_two) { create(:user) }
+          before do
+            @new_email = 'new@email.com'
+            patch user_path(user_two.id),
+                  params: { email: 'new@email.com' },
+                  headers: create_auth_header(user)
+          end
+          it 'should return a 401 status' do
+            expect(response).to have_http_status(:unauthorized)
+          end
+          it 'should have an empty body' do
+            expect(response.body).to eq ''
+          end
+          it 'should not edit a user' do
+            expect(User.find(user_two.id).email).to_not eq @new_email
+          end
         end
       end
     end
-    context 'who is not an admin' do
-      let!(:user) { create(:user) }
-      context 'editing their own account' do
-        before do
-          @new_email = "new@email.com"
-          patch user_path(user.id),
-            params: { email: 'new@email.com' },
-            headers: create_auth_header(user)
-        end
-        it 'should return a 200 status' do
-          expect(response).to have_http_status(:ok)
-        end
-        it 'should have the updated user in the body' do
-          expect(JSON(response.body)['data']['attributes']['email']).to eq @new_email
-        end
-        it 'should update a user' do
-          expect(User.find(user.id).email).to eq @new_email
-        end
+    context 'with unauthenticated user' do
+      let(:user) { create(:user) }
+      before do
+        delete user_path(user.id)
       end
-      context "updating someone else's account" do
-        let!(:user_two) { create(:user) }
-        before do
-          @new_email = "new@email.com"
-          patch user_path(user_two.id),
-            params: { email: 'new@email.com' },
-            headers: create_auth_header(user)
-        end
-        it 'should return a 401 status' do
-          expect(response).to have_http_status(:unauthorized)
-        end
-        it 'should have an empty body' do
-          expect(response.body).to eq ''
-        end
-        it 'should not edit a user' do
-          expect(User.find(user_two.id).email).to_not eq @new_email
-        end
+      it 'should return a 401 status' do
+        expect(response).to have_http_status(:unauthorized)
       end
     end
-  end
-  context 'with unauthenticated user' do
-    let(:user) { create(:user) }
-    before do
-      delete user_path(user.id)
-    end
-    it 'should return a 401 status' do
-      expect(response).to have_http_status(:unauthorized)
-    end
-  end
   end
   describe 'DELETE #destroy' do
     context 'with an authenticated user' do
@@ -202,7 +201,6 @@ RSpec.describe 'Users', type: :request do
           it 'should not delete a user' do
             expect(User.count).to equal @user_count
             expect(User.all).to include user
-
           end
         end
       end
