7c55e9c8a90d251d66fadb114ba6def9adf9e084
meryldominguez <meryldominguez@gmail.com>
2024-01-01 18:10:04 -0600

    validates uniqueness of profiles

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_01_01_180014) do
+ActiveRecord::Schema[7.1].define(version: 2024_01_01_234622) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -27,6 +27,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_01_01_180014) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["instrument_id"], name: "index_profiles_on_instrument_id"
+    t.index ["user_id", "instrument_id"], name: "index_profiles_on_user_id_and_instrument_id", unique: true
     t.index ["user_id"], name: "index_profiles_on_user_id"
   end
 
